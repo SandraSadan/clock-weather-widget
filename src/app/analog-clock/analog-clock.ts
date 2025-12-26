e841c09ee6a29abc7db9fa3a0269a5141de78241
SandraSadan1 <ss5282g@gre.ac.uk>
2025-12-26 18:10:51 +0000

    Initial clock and weather app setup

---
+++ src/app/analog-clock/analog-clock.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-analog-clock',
+  imports: [],
+  templateUrl: './analog-clock.html',
+  styleUrl: './analog-clock.scss'
+})
+export class AnalogClock {
+
+}
